730c8bf59337a646248d89301d15202e71bc8a18
MutabPato <patrickmtbz@gmail.com>
2025-05-14 05:02:36 +0300

    grouping by ratings

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,4 +1,4 @@
 SELECT booking.id, users.first_name FROM booking INNER JOIN users ON booking.user_id=users.id;
-SELECT property.name, review.comment FROM property LEFT JOIN review ON property.id=review.property_id;
+SELECT property.name, review.comment FROM property LEFT JOIN review ON property.id=review.property_id ORDER BY rating;
 SELECT users.first_name, booking.id FROM users FULL OUTER JOIN booking ON users.id = booking.
 user_id;
