4d649d0e7d457ef9673329cf0bb43c6815085681
MutabPato <patrickmtbz@gmail.com>
2025-05-14 07:08:39 +0300

    Aggregations and Window Functions

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,2 +1,8 @@
 SELECT user_id, COUNT(*) AS total_bookings FROM booking GROUP BY user_id;
-SELECT property_id, RANK() OVER (PARTITION BY property_id) AS property_rank FROM booking;
+WITH booking_rows AS (
+    SELECT *,
+    ROW_NUMBER() OVER (PARTITION BY property_id ORDER BY start_date) AS row_num
+    FROM BOOKING
+)
+SELECT property_id, COUNT(*) AS total_bookings,
+RANK() OVER (ORDER BY COUNT(*)) AS property_rank FROM booking_rows GROUP BY property_id;
