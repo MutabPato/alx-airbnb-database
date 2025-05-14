4ca60c690f1bc2b99ebb4c2d59b25f883d6fa1f5
MutabPato <patrickmtbz@gmail.com>
2025-05-14 09:10:45 +0300

    implementing indexes for optimization

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,3 +1,7 @@
+EXPLAIN ANALYZE SELECT booking.id, users.first_name FROM booking INNER JOIN users ON booking.user_id=users.id;
+
 CREATE INDEX idx_first_name ON users(first_name);
 CREATE INDEX idx_property_name ON property(name);
 CREATE INDEX idx_start_date ON booking(start_date);
+
+EXPLAIN ANALYZE SELECT booking.id, users.first_name FROM booking INNER JOIN users ON booking.user_id=users.id;
