c3401a603ca236670914830a3143d11caa67175e
MutabPato <patrickmtbz@gmail.com>
2025-05-16 07:52:19 +0300

    rewriting query using WHERE and AND

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,4 +1,14 @@
-SELECT booking.*, users.first_name, users.last_name, users.email, users.phone_number, property.name, property.description, property.location, payment.payment_method FROM booking 
-LEFT JOIN users ON booking.user_id = users.id 
-LEFT JOIN property ON booking.property_id = property.id
-LEFT JOIN payment ON booking.id = payment.booking_id;
+SELECT 
+booking.*,
+users.first_name,
+users.last_name, 
+users.email, 
+users.phone_number, 
+property.name, 
+property.description, 
+property.location, 
+payment.payment_method 
+FROM booking, users, property, payment
+WHERE booking.user_id = users.id 
+AND booking.property_id = property.id
+AND booking.id = payment.booking_id;
